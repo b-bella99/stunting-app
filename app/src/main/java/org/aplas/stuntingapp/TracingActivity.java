28a6d34d63f43a6950aef1d1e618f3fcf3149230
b-bella99 <bella.setyowati99@gmail.com>
2023-03-06 08:35:55 +0700

    Edit layout dashboard, food help, nutrition, and tracing stunting

---
+++ app/src/main/java/org/aplas/stuntingapp/TracingActivity.java
@@ -0,0 +1,14 @@
+package org.aplas.stuntingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TracingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracing);
+    }
+}
